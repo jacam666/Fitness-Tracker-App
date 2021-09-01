0593e3cff903008baf846ed28525235ef48b5ce7
jacam666 <ja6cam@gmail.com>
2021-09-01 20:30:36 +0100

    :added exercise class : added workout class

---
+++ Fitness Tracker App/Data/Workout.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fitness_Tracker_App.Data
+{
+   public class Workout
+    {
+        public int AmountOfSets;
+        public int AmountOfReps;
+        public DateTime Date;
+    }
+}

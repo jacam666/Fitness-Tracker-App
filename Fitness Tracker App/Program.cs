e49eb1986614148faf7f2c0b0a86d9ded06832ce
jacam666 <ja6cam@gmail.com>
2021-09-02 18:28:13 +0100

    :added consolewriteline informing user of how many sets and reps they have completed and on what date

--- Fitness Tracker App/Program.cs
+++ Fitness Tracker App/Program.cs
@@ -19,7 +19,9 @@ namespace Fitness_Tracker_App
             workout.Date = DateTime.Today;
             Console.WriteLine("Pleas enter the amount of Reps completed :");
             Console.WriteLine($"The amount of Reps you completed for this exercise are {workout.AmountOfReps}:");
-            
+            Console.WriteLine("Please enter the amount of Sets completed :");
+            Console.WriteLine($"The amount of Sets you completed for this exercise are {workout.AmountOfSets}:");
+            Console.WriteLine($"On the {workout.Date} you completed {workout.AmountOfSets} sets of {workout.AmountOfReps} Reps");
 
 
 

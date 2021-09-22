70cc87135ac4dddf999dfd253d9f5fe5575aeeca
jacam666 <ja6cam@gmail.com>
2021-09-22 20:06:41 +0100

    : added dropdown box for chest exercises.

--- FitnessWebApp/Startup.cs
+++ FitnessWebApp/Startup.cs
@@ -15,6 +15,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Syncfusion.Blazor;
 
 namespace FitnessWebApp
 {
@@ -42,6 +43,7 @@ namespace FitnessWebApp
             services.AddDatabaseDeveloperPageExceptionFilter();
             services.AddSingleton<WeatherForecastService>();
             services.AddSingleton<DataService>();
+            services.AddSyncfusionBlazor();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.

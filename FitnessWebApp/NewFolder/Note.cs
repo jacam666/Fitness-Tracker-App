9216054500e30768475ab74fa284b63651d02ae9
jacam666 <ja6cam@gmail.com>
2022-01-18 19:47:10 +0000

    :neatened up code, changed the font to white so we could see it over the new black background : using a tutorial to create a page to keep notes regarding my coding, whilst practicing onclick binding

---
+++ FitnessWebApp/NewFolder/Note.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FitnessWebApp.NewFolder
+{
+    public class Note
+    {
+        public string Message { get; }
+
+        public DateTimeOffset Created { get; }
+
+        public Note(string message)
+        {
+            Message = message;
+            Created = DateTimeOffset.Now;
+        }
+    }
+}
